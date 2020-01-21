4410a083992f86bd5f074cc2250556cc34046cc4
naz_liton <nazrulliton@icloud.com>
2020-01-21 10:00:41 +0000

    models created

--- src/main/java/com/codeclan/example/Company/models/Project.java
+++ src/main/java/com/codeclan/example/Company/models/Project.java
@@ -1,4 +1,48 @@
 package com.codeclan.example.Company.models;
 
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "projects")
+
 public class Project {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "project_name")
+    private String projectName;
+    @Column (name = "duration")
+    private int duration;
+
+
+    public Project(Long id, String projectName, int duration) {
+        this.id = id;
+        this.projectName = projectName;
+        this.duration = duration;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getProjectName() {
+        return projectName;
+    }
+
+    public void setProjectName(String projectName) {
+        this.projectName = projectName;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
 }
